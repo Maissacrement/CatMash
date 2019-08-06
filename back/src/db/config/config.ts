4c536c62746b4ac78c572eb841215edc85eae0dc
mario <emmariodelar@gmail.com>
2019-08-06 05:07:16 +0200

    fix lint

--- back/src/db/config/config.ts
+++ back/src/db/config/config.ts
@@ -1,14 +1,16 @@
-export interface redisTs {
+export interface IRedisTs {
   port: number;
   host: string;
   options?: {} | undefined;
 }
 
-const config: redisTs = {
-  port: parseInt(`${process.env.REDIS_PORT}`)
-    ? parseInt(`${process.env.REDIS_PORT}`)
-    : 6379,
-  host: process.env.REDIS_ADDRESS ? `${process.env.REDIS_ADDRESS}` : "127.0.0.1"
+const config: IRedisTs = {
+  host: process.env.REDIS_ADDRESS
+    ? `${process.env.REDIS_ADDRESS}`
+    : "127.0.0.1",
+  port: parseInt(`${process.env.REDIS_PORT}`, 10)
+    ? parseInt(`${process.env.REDIS_PORT}`, 10)
+    : 6379
 };
 
 export default config;
