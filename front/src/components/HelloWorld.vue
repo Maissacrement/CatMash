c8319ec1016d379e93b952edddf2e15c7aa7bcd8
mario <emmariodelar@gmail.com>
2019-08-07 16:32:44 +0200

    init template

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="main">
+    <div class="left"></div>
+    <div class="right"></div>
+    <div class="bottom"></div>
   </div>
 </template>
 
@@ -14,9 +17,40 @@ export default class HelloWorld extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+* {
+  box-sizing: border-box;
+}
 .main {
   width: 100%;
   height: 100vh;
-  background: red;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: 1fr 7%;
+  padding: 6px 6px 6px 6px;
+  .right, .left {
+    grid-row-start: 1;
+    grid-row-end: span 2;
+  }
+  .left {
+    background-color: #eceeee;
+  }
+  .right {
+    background-color: #f5f5f5;
+  }
+  .bottom {
+    position: absolute;
+    bottom: 6px;
+    background-color: #f5f5f5;
+    width: calc(100% - 12px);
+    max-width: 300px;
+    height: 42px;
+    border-width: 6px 6px 0 6px;
+    border-radius: 10px 10px 0 0;
+    border-color: #eceeee;
+    border-style: solid;
+    @media (min-width: 300px) {
+      left: calc((100% - 300px) / 2);
+    }
+  }
 }
 </style>
